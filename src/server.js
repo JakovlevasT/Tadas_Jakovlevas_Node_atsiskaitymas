233f197ab6b93c3e43a7729e95a593163c44b9f8
TadasJ <tadasjakovlevas@gmail.com>
2024-01-06 20:17:25 +0200

    server is On

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { testConnection } = require('./helper');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// use Routers
+
+// testConnection();
+
+app.listen(PORT, () => {
+  console.log(`Server runing on http://localhost:${PORT}`);
+});
